ec37e7b771871c5c0ce5a4d759e32914f0c5d3fd
Oleksii <qizzup@gmail.com>
2018-11-06 17:01:32 +0100

    turned off requests and added seeds

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -10,32 +10,32 @@ class Api::V1::LocationsController < ApplicationController
 
   # GET /locations/1
   def show
-    render json: @location
+    # render json: @location
   end
 
   # POST /locations
   def create
-    @location = Location.new(location_params)
+    # @location = Location.new(location_params)
 
-    if @location.save
-      render json: @location, status: :created, location: api_v1_location_url(@location)
-    else
-      render json: @location.errors, status: :unprocessable_entity
-    end
+    # if @location.save
+    #   render json: @location, status: :created, location: api_v1_location_url(@location)
+    # else
+    #   render json: @location.errors, status: :unprocessable_entity
+    # end
   end
 
   # PATCH/PUT /locations/1
   def update
-    if @location.update(location_params)
-      render json: @location
-    else
-      render json: @location.errors, status: :unprocessable_entity
-    end
+    # if @location.update(location_params)
+    #   render json: @location
+    # else
+    #   render json: @location.errors, status: :unprocessable_entity
+    # end
   end
 
   # DELETE /locations/1
   def destroy
-    @location.destroy
+    # @location.destroy
   end
 
   private
